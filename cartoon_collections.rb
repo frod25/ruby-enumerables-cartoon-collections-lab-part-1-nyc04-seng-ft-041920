bb4f8274e168242a3f87ab6a246c392b67bf52c4
Felix Rodriguez <frod25@gmail.com>
2020-04-29 17:29:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index { |element|
-    puts "Hello #{element}!"
+  array.each_with_index { |index, element|
+    puts "#{index + 1}. #{element}!"
   }
 end
